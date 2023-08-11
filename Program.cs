92ff0906deb7469bac40fc7130e9c5c37db7ee4c
Anders Bo Pedersen <ABP@ao.dk>
2023-08-11 14:37:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace AsymmetriskKrypteringsOpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of RSAParameterKey calss
+            var rsaParameters = new RSAParameterKey();
+
+            string testText = "En meget hemmelig tekst, der ikke må læses af andre";
+
+            try
+            {
+                rsaParameters.AssingningNewKey();
+
+                var rsaEncrypted = rsaParameters.Encrypt(Encoding.UTF8.GetBytes(testText));
+                var rsaDecrypted = rsaParameters.Decrypt(rsaEncrypted);
+
+                var rsaDecryptedText = Encoding.UTF8.GetString(rsaDecrypted); 
+
+                Console.WriteLine("RSA parameter");
+                Console.WriteLine();
+
+                foreach (var item in rsaParameters.privateKey.GetType().GetFields())
+                {
+                    byte[] value = (byte[])item.GetValue(rsaParameters.privateKey);
+                    Console.WriteLine($"{item.Name}: \n{BitConverter.ToString(value)}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Første tekst: \r\n" + testText);
+                Console.WriteLine();
+                Console.WriteLine("Kryptered tekst: \r\n" + Convert.ToBase64String(rsaEncrypted));
+                Console.WriteLine();
+                Console.WriteLine("Dekryptered tekst: \r\n" + rsaDecryptedText);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Der skete den fejl!"); 
+            }
+        }
+    }
+}
---
+++ RSAParameterKey.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymmetriskKrypteringsOpg
+{
+    public class RSAParameterKey
+    {
+        public RSAParameters publicKey;
+        public RSAParameters privateKey;
+
+        /// <summary>
+        /// Create a new instance of RSA,
+        /// to generate public and private key 
+        /// </summary>
+        public void AssingningNewKey()
+        {            
+            var rsa = RSA.Create();
+
+            rsa.KeySize = 2048;
+
+            publicKey = rsa.ExportParameters(false);
+            privateKey = rsa.ExportParameters(true);
+        }
+
+
+        /// <summary>
+        /// Method for encrypting the text,
+        /// Create byte arrays to hold the encrypted text,
+        /// Create a new instance of RSA,
+        /// Import the RSA publicKey information,
+        /// Encrypt the byte array and sets OAEP padding
+        /// </summary>
+        /// <param name="textToEncrypt"></param>
+        /// <returns></returns>
+        public byte[] Encrypt(byte[] textToEncrypt)
+        {
+            byte[] encryptedText;
+
+            try
+            {
+                var rsa = RSA.Create();
+
+                rsa.ImportParameters(publicKey);
+                encryptedText = rsa.Encrypt(textToEncrypt, RSAEncryptionPadding.OaepSHA256);
+
+                return encryptedText;
+            }
+            catch (CryptographicException e)
+            {
+                Debug.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Method for decrypting the cipher,
+        /// Create byte arrays to hold the decrypted text,
+        /// Create a new instance of RSA,
+        /// Import the RSA privateKey information,
+        /// Decrypt the byte array and sets OAEP padding
+        /// </summary>
+        /// <param name="textToEncrypt"></param>
+        /// <returns></returns>
+        public byte[] Decrypt(byte[] textToEncrypt)
+        {
+            byte[] decryptedText;
+
+            try
+            {
+                var rsa = RSA.Create();
+
+                rsa.ImportParameters(privateKey);
+                decryptedText = rsa.Decrypt(textToEncrypt, RSAEncryptionPadding.OaepSHA256);
+
+                return decryptedText;
+            }
+            catch (CryptographicException e)
+            {
+                Debug.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fs"></param>
+        /// <param name="value"></param>
+        private static void FileStreamForKey(FileStream fs, string value)
+        {
+            byte[] text = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(text, 0, text.Length);
+        }
+    }
+}
